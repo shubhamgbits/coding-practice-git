72317609351f2376a791adb0eebeed19ba7cef1e
Shubham Gupta <shubham@Shubhams-MacBook-Pro.local>
2024-07-01 22:20:22 +0530

    resolve

--- Python/gemini/agent.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCVCMs9WliN-AKOwWfH-PYaZ-l0Vv_DVw4")
-# The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
